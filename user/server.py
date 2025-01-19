128f00091a1cdc11abca7f89287d8a059476f456
thomas devasia <tdevasia1997@gmail.com>
2025-01-19 13:55:13 +0530

    add endpoint collect altest news

--- user/server.py
+++ user/server.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-import httpx
+import json
+# import httpx
 import psycopg2
 import os
+import pika
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER")
+RABBITMQ_PASS = os.getenv("RABBITMQ_PASS")
 AUTH_SERVICE_URL = "http://auth-service:8001"
 
 
@@ -25,6 +29,7 @@ def get_user_info(username):
         cursor.execute(
             f"SELECT id, username, name, email FROM users WHERE username = '{username}'")
         user = cursor.fetchone()
+        conn.close()
         return user
     except Exception as e:
         print(e)
@@ -50,10 +55,52 @@ def update_news_selection(user_id, news_selection):
             cursor.execute(
                 "INSERT INTO news_topic (user_id, topics) VALUES (%s, %s)", (user_id, news_selection))
         conn.commit()
+        conn.close()
         return True
     except Exception as e:
         print(e)
         return False
+
+
+def get_all_userid() -> list[str]:
+    try:
+        conn = psycopg2.connect(
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM users")
+        users = cursor.fetchall()
+        conn.close()
+        return users
+    except Exception as e:
+        print(e)
+        return None
+
+
+def get_news_topic(user_id: str) -> list[str]:
+    try:
+        conn = psycopg2.connect(
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT topics from news_topic where user_id = %s", (user_id,))
+        topics = cursor.fetchone()
+        conn.close()
+        if topics:
+            return topics[0]
+        else:
+            return ([])
+    except Exception as e:
+        print(e)
+        return None
 
 
 app = FastAPI()
@@ -120,7 +167,7 @@ def get_news(request: Request, user_name: str):
         if news_selection:
             return {"topics": news_selection[0], "username": user_info[1]}
         else:
-            return {"message": "No news selection found"}
+            return {"message": "No news selection found", "topics": [], "username": user_info[1]}
     except Exception as e:
         print(e)
         return {"message": "Error fetching news selection"}
@@ -139,4 +186,24 @@ async def news_action(request: Request, user_name: str):
         else:
             return {"message": "News selection update failed"}
 
-    response = {"Info": "user service", "action": "news"}
+
+@app.get("/get_latest_news")
+async def get_latest_news():
+    users = get_all_userid()
+    for user in users:
+        news_topic = get_news_topic(user[0])
+        payload = json.dumps({"user_id": user[0], "topics": news_topic})
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='rabbitmq',
+                credentials=pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
+            ))
+            channel = connection.channel()
+            channel.queue_declare(queue='user.newsCollection', durable=True, auto_delete=True)
+            channel.basic_publish(exchange='', routing_key='user.newsCollection', body=payload)
+            connection.close()
+        except Exception:
+            response = {"message": "Error connecting to RabbitMQ"}
+            return JSONResponse(content=response, status_code=500)
+    response = {"message": "News collection successful queued"}
+    return JSONResponse(content=response, status_code=200)
